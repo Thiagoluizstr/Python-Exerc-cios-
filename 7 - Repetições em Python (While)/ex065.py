e9f92635de4ebad78fa8bbb8ed002523b45f4a57
Thiagoluizstr <thiagoluizstr@hotmail.com>
2021-09-17 18:52:45 -0300

    Atualização de exercício
    Ultimo exercício do modulo dois de Python

--- 7 - Repetições em Python (While)/ex065.py
+++ 7 - Repetições em Python (While)/ex065.py
@@ -1,39 +1,27 @@
-from time import sleep
-
 print('='*30)
-print('{:^30}'.format('BANCO lIMA'))
+print('{:^30}'.format('BANCO LIMA'))
 print('='*30)
 
+valor = int(input('Valor do saque: R$ '))
+total = valor
+cédula = 100
+cétotal = 0
 
 while True:
-    print('Cédulas disponivés no momento')
-    sleep(1)
-    print('[R$ 5] [R$ 10]')
-    print('[R$ 20] [R$ 50]')
-    print('[R$ 100] [R$ 200]')
-
-    operação = ' '
-    valor = 0
-    while operação not in 'N':
-        print('=' * 30)
-        valor = float(input('Que valor deseja sacar? R$ '))
-        if valor == 5:
-            print('Aquarde um momento', end='')
-            sleep(1)
-            print('.', end='')
-            sleep(1)
-            print('.', end='')
-            sleep(1)
-            print('.', end='')
-            sleep(1)
-            print('.')
-            sleep(1)
-            print('Pode Retirar o dinheiro')
-
-        operação = str(input('Deseja seguir com a operção: [S/N] ')).strip().upper()[0]
-        sleep(1)
-
-    print('FIM DA OPERAÇÃO')
-    print('Obrigado e volte sempre')
-    break
+    if total >= cédula:
+        total -= cédula
+        cétotal += 1
+    else:
+        print(f'Total de {cétotal} cédulas de R${cédula}')
+        if cédula == 100:
+            cédula = 50
+        elif cédula == 50:
+            cédula = 20
+        elif cédula == 20:
+            cédula = 10
+        elif cédula == 10:
+            cédula = 1
+        cétotal = 0
+        if total == 0:
+            break
 
