417288048bcc9986c5c567f78dff0a49672eddd6
Thiagoluizstr <75076981+Thiagoluizstr@users.noreply.github.com>
2024-01-19 16:39:13 -0300

    Update Python 001.py
    Atualização de README

--- 1 - Primeiro Passo com Python/Python 001.py
+++ 1 - Primeiro Passo com Python/Python 001.py
@@ -1,6 +1,6 @@
 '''print('Olá, me chamo Thiago Luiz.')
 print('Sou formado em Eng. Elétrica com enfâse em Eletrônica')
-print('Atualmente curso Ciência de Dados e Big Data Analytics')
+print('Atualmente curso Engenharia de Computação')
 print('Estou subindo alguns arquivos aqui no Github para mostrar meu aprendizado.')
 print('Em breve estarei subindo alguns arquivos de projetos que estou trabalhando.')
 print('Até breve.')'''
